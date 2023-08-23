e4e0e0552e8b1adbdd0dac01e7c0846d6f02b61e
fsjal-sys <2kpilot2k@gmail.com>
2023-08-23 18:33:36 -0400

    Initial Commit

---
+++ indeedbot/bot.py
@@ -0,0 +1,15 @@
+from utils import *
+from time import sleep
+from dotenv import load_dotenv
+
+driver = driver_setup()
+
+driver.get("https://www.google.com")
+
+def login():
+    pass
+
+def main():
+    pass
+
+main()
